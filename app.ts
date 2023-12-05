a14b0da52305edcecfcdb86f90c5831c6bf72ac8
unknown <contact.harsht67@gmail.com>
2023-12-05 15:27:29 +0530

    Project init

---
+++ app.ts
@@ -0,0 +1,30 @@
+import { NextFunction, Request, Response } from "express";
+
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors({
+    origin: process.env.ORIGIN
+}))
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+});
+
+app.get("/test", (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).json({
+        success: true,
+        message: "API is working"
+    })
+})
+
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+    const err = new Error(`Route ${req.url} not found!`) as any;
+    err.statusCode = 404;
+    next(err);
+})
